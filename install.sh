00bfc6dbf265d4d971ebe259ef16f577d55daffa
Jxck <block.rxckin.beats@gmail.com>
2012-06-18 12:41:37 +0900

    [install] fix order

--- install.sh
+++ install.sh
@@ -47,6 +47,7 @@ fi
 # install nodebrew
 wget https://raw.github.com/hokaccha/nodebrew/master/nodebrew
 perl nodebrew setup
+rm nodebrew
 NODE_PATH='export PATH=$HOME/.nodebrew/current/bin:$PATH'
 eval $NODE_PATH
 echo $NODE_PATH >> ~/.bashrc
@@ -61,14 +62,15 @@ nodebrew use v0.6.11
 # install rbenv
 git clone git://github.com/sstephenson/rbenv.git .rbenv
 RBENV_PATH='export PATH=$HOME/.rbenv/bin:$PATH'
-eval $RBENV_PATH
-echo $RBENV_PATH >> ~/.bashrc
-eval '$(rbenv init -)'
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 # install ruby-build
 mkdir -p ~/.rbenv/plugins
 cd ~/.rbenv/plugins
 git clone git://github.com/sstephenson/ruby-build.git
+# set path & init
+eval $RBENV_PATH
+echo $RBENV_PATH >> ~/.bashrc
+rbenv init -
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 # install ruby
 rbenv install 1.9.3-p194
 rbenv global 1.9.3-p194
