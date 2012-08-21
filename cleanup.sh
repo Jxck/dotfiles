49ab6ea3a70a8b2041ce26f759cdbd5bb605549d
Jxck <block.rxckin.beats@gmail.com>
2012-08-21 11:41:53 +0900

    add cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# http://library.gnome.org/admin/system-admin-guide/stable/appendixa-0.html.ja
+# http://uguisu.skr.jp/Windows/setting.html
+
+# ignore
+# .config/
+# .gvfs/
+# .local/
+
+target_files="\
+.cache/
+.dbus/
+.esd_auth
+.evolution/
+.gconf/
+.gconfd/
+.gnome/
+.gnome2_private/
+.goutputsetream-*
+.gstreamer-0.10/
+.gtk-bookmarks
+.nautilus/
+.pulse/
+.pulse-cookie
+.recently-used.xbel*
+.swt/
+.thumbnails/
+.w3m/
+.xsession-errors"
+
+
+for target in $target_files
+do
+  rm -rf "$HOME/$target"
+done
+
+
