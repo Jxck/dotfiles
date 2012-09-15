2b0cea1e814feb795fa377a48647e1d53d321d9e
Jxck <block.rxckin.beats@gmail.com>
2012-09-15 17:08:49 +0900

    commect out rbenv

--- install.sh
+++ install.sh
@@ -60,23 +60,23 @@ nodebrew use v0.6.11
 ###################
 
 # install rbenv
-git clone git://github.com/sstephenson/rbenv.git .rbenv
-RBENV_PATH='export PATH=$HOME/.rbenv/bin:$PATH'
-# install ruby-build
-mkdir -p ~/.rbenv/plugins
-cd ~/.rbenv/plugins
-git clone git://github.com/sstephenson/ruby-build.git
-# set path & init
-eval $RBENV_PATH
-echo $RBENV_PATH >> ~/.bashrc
-rbenv init -
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-# install ruby
-rbenv install 1.9.3-p194
-rbenv global 1.9.3-p194
-# install rails & bundler
-rbenv exec gem install bundler rails
-exec $shell
-
-echo 'alias bundle-install="bundle install --path vendor/bundle"' >> ~/.bashrc
-echo 'alias rails="bundle exec rails"' >> ~/.bashrc
+#git clone git://github.com/sstephenson/rbenv.git .rbenv
+#RBENV_PATH='export PATH=$HOME/.rbenv/bin:$PATH'
+## install ruby-build
+#mkdir -p ~/.rbenv/plugins
+#cd ~/.rbenv/plugins
+#git clone git://github.com/sstephenson/ruby-build.git
+## set path & init
+#eval $RBENV_PATH
+#echo $RBENV_PATH >> ~/.bashrc
+#rbenv init -
+#echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+## install ruby
+#rbenv install 1.9.3-p194
+#rbenv global 1.9.3-p194
+## install rails & bundler
+#rbenv exec gem install bundler rails
+#exec $shell
+#
+#echo 'alias bundle-install="bundle install --path vendor/bundle"' >> ~/.bashrc
+#echo 'alias rails="bundle exec rails"' >> ~/.bashrc
