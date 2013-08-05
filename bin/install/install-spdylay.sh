63d5103c9baa9fe218f3b62b6d53d1c5b22d981d
Jxck <block.rxckin.beats@gmail.com>
2013-08-06 02:40:43 +0900

    [bin] add install-nghttp2 to install-spdylay.sh

--- bin/install/install-spdylay.sh
+++ bin/install/install-spdylay.sh
@@ -23,8 +23,20 @@ sudo apt-get install -y \
 sudo apt-get install -y python-software-properties
 sudo apt-get update
 
+### install gcc4.7
+# http://charette.no-ip.com:81/programming/2011-12-24_GCCv47/
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
-sudo apt-get install gcc-4.7
-
+sudo apt-get install -y gcc-4.7 g++-4.7
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7
 sudo update-alternatives --list gcc
+sudo update-alternatives --config gcc
+
+#git clone https://github.com/tatsuhiro-t/nghttp2.git
+#cd nghttp2
+#autoreconf -i
+#automake
+#autoconf
+#./configure
+#make
