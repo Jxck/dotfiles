0dfe2e6b221c9b6f10aba12ce19296752ee820bb
Jxck <block.rxckin.beats@gmail.com>
2013-08-06 01:34:31 +0900

    [bin] add install-spdylay.sh

---
+++ bin/install/install-spdylay.sh
@@ -0,0 +1,21 @@
+sudo apt-get install -y \
+  cunit                 \
+  autoconf              \
+  automake              \
+  autotools-dev         \
+  libtool               \
+  pkg-config            \
+  zlib1g-dev            \
+  libcunit1-dev         \
+  libssl-dev            \
+  libxml2-dev           \
+  libevent-dev          \
+  libevent-openssl
+
+git clone https://github.com/tatsuhiro-t/spdylay.git
+cd spdyaly
+#autoreconf -i
+#automake
+#autoconf
+#./configure
+#make
