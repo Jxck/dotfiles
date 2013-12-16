e886e4da54fe5dc7fac7d744d1c2c275d142fc64
master <master@jubuntu.(none)>
2013-12-16 23:22:35 +0900

    [vim] update go test template

--- .vim/template/test.go
+++ .vim/template/test.go
@@ -5,7 +5,24 @@ import (
 )
 
 func Test(t *testing.T) {
-	if a != b {
-		t.Errorf("got %v\nwant %v", a, b)
+	if actual != expected {
+		t.Errorf("\ngot  %v\nwant %v", actual, expected)
+	}
+}
+
+var testcase = []struct {
+	in, out string
+}{
+	{"a", "b"},
+	{"a", "b"},
+}
+
+func TestParameter(t *testing.T) {
+	for _, tc := range testcase {
+		in := tc.in
+		out := tc.out
+		if actual != expected {
+			t.Errorf("\ngot  %v\nwant %v", actual, expected)
+		}
 	}
 }
