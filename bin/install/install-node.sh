95fb95d7005ad98286a3ef618a536f6343c1cb63
Jxck <block.rxckin.beats@gmail.com>
2014-10-03 00:21:10 +0900

    [bin] fix install node

--- bin/install/install-node.sh
+++ bin/install/install-node.sh
@@ -8,7 +8,7 @@ perl nodebrew setup
 rm nodebrew
 NODE_PATH='export PATH=$HOME/.nodebrew/current/bin:$PATH'
 eval $NODE_PATH
-echo $NODE_PATH >> ~/.bashrc
+
 # install node
 nodebrew install stable
 nodebrew use stable
