72a0478a10ef053ceac9745e4c2dd17a47ba545c
Jxck <block.rxckin.beats@gmail.com>
2014-10-03 00:21:49 +0900

    [conf] add proxy.sh

---
+++ bin/proxy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo -n "user: "
+read user
+
+echo -n "pass: "
+read pass
+
+echo -n "host: "
+read host
+
+echo -n "port: "
+read port
+
+echo "http://$user:$pass@$host:$port"
+confpath="$HOME/dotfiles/conf"
+
+for f in `\ls -A $confpath`
+do
+  cat $confpath/$f |
+  sed -e "s/{{user}}/$user/g" |
+  sed -e "s/{{pass}}/$pass/g" |
+  sed -e "s/{{host}}/$host/g" |
+  sed -e "s/{{port}}/$port/g" > ~/$f
+done
+
+if [ `uname` = "Darwin" ]; then
+  rm ~/apt.conf
+elif [ `uname` = "Linux" ]; then
+  sudo mv ~/apt.conf /etc/apt/apt.conf
+fi
--- conf/.profile
+++
@@ -1,6 +0,0 @@
-export http_proxy="http://user:pass@host:port"
-export https_proxy="https://user:pass@host:port"
-export HTTP_PROXY=$http_proxy
-export HTTPS_PROXY=$https_proxy
-export all_proxy=$http_proxy
-export ALL_PROXY=$http_proxy
