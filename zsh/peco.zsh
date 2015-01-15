b2bf30f9c1261fc5a0d68f556fd0c4b8c5c78612
Jxck <block.rxckin.beats@gmail.com>
2015-01-15 15:22:26 +0900

    [zsh] ignore dups in peco-select-history

--- zsh/peco.zsh
+++ zsh/peco.zsh
@@ -24,7 +24,7 @@ function peco-select-history() {
   else
     tac="tail -r"
   fi
-  BUFFER=$(history -n 1 | eval $tac | peco --query "$LBUFFER")
+  BUFFER=$(history -n 1 | eval $tac | awk '!a[$0]++' | peco --query "$LBUFFER")
   CURSOR=$#BUFFER
   zle clear-screen
 }
