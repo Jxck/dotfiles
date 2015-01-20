826abe4d6e417ccd0fe5247a51f0204ce401a04b
Jxck <block.rxckin.beats@gmail.com>
2015-01-20 13:44:03 +0900

    [zsh] cleanup peco.zsh

--- zsh/peco.zsh
+++ zsh/peco.zsh
@@ -1,14 +1,17 @@
 alias psp="ps aux | peco"
 alias killp="ps aux | peco | awk '{print \$2}' | xargs kill -9"
 
+# move to directory found with peco
 function cdp() {
   cd $(find . -maxdepth 4 -type d ! -path "*/.*" | peco)
 }
 
+# move to GPATH/src
 function cdgo() {
   cd $GOPATH/src && cdp
 }
 
+# open file found with peco in vim
 function vimp() {
   if git ls-files; then
     vim `git ls-files | peco`
@@ -17,6 +20,7 @@ function vimp() {
   fi
 }
 
+# ignore duplicates from history
 function peco-select-history() {
   local tac
   if which tac > /dev/null; then
