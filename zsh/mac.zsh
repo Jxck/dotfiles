418287509b3d11daa43e072341a4ef7357193e3d
Jxck <block.rxckin.beats@gmail.com>
2015-01-20 19:22:19 +0900

    [zsh] pb support file as argument

--- zsh/mac.zsh
+++ zsh/mac.zsh
@@ -10,8 +10,16 @@ export LSCOLORS=exfxcxdxbxegedabagacad
 export LS_COLORS='di=33:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 
 # pb
-alias pb="pbcopy"
 alias pbp="pbpaste"
+
+#alias pb="pbcopy"
+function pb() {
+  if [ -t 0 ]; then
+    cat $1 | pbcopy
+  else
+    pbcopy < /dev/stdin
+  fi
+}
 
 # alias
 alias ls="ls -A1hvG"
