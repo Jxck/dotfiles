5480cea1e9bb65bacf01fa4443c712f9fabbafe3
Jxck <block.rxckin.beats@gmail.com>
2015-04-01 19:48:50 +0900

    [install] add docker-install.sh

---
+++ install/install-docker.sh
@@ -0,0 +1,50 @@
+###################
+# intall docker
+###################
+
+## check os
+if [ ! `uname` = "Linux" ]; then
+  echo "run this command on Ubuntu"
+  exit 1
+fi
+
+## install docker
+echo -n "install docker? (y/n): "
+read install
+
+if [ $install = "y" ]; then
+  echo "run wget -qO- https://get.docker.com/ | sh"
+  echo
+  # wget -qO- https://get.docker.com/ | sh
+  echo
+fi
+
+## add current user to docker group
+echo -n "add current user to docker group? (y/n): "
+read usergroup
+
+if [ $usergroup = "y" ]; then
+  echo "sudo usermod -aG docker `whoami`"
+  echo
+  # sudo usermod -aG docker `whoami`
+  echo
+fi
+
+## edit docker file
+echo -n "edit docker file? (y/n): "
+read editdocker
+
+if [ $editdocker = "y" ]; then
+  echo "sudo vim /etc/default/docker"
+  sudo vim /etc/default/docker
+fi
+
+## restart docker
+echo -n "restart docker? (y/n): "
+read restart
+
+if [ $restart = "y" ]; then
+  echo "sudo services docker docker"
+  echo
+  sudo service docker restart
+fi
