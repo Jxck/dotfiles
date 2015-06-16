f6baaf188070838deff72319d758b017dc5f8b73
Jxck <block.rxckin.beats@gmail.com>
2015-06-16 11:07:02 +0900

    [zsh] replace double-width char in single-width while pbp

--- zsh/mac.zsh
+++ zsh/mac.zsh
@@ -10,7 +10,14 @@ export LSCOLORS=exfxcxdxbxegedabagacad
 export LS_COLORS='di=33:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 
 # pb
-alias pbp="pbpaste"
+#alias pbp="pbpaste"
+function pbp() {
+  pbpaste | sed '
+    s/”/"/g
+    s/“/"/g
+    s/–/-/g
+  ' | sed "s/’/'/g"
+}
 
 #alias pb="pbcopy"
 function pb() {
