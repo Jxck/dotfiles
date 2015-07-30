f851366f66352701aafc7222954473380ba99a16
Jxck <block.rxckin.beats@gmail.com>
2015-07-30 13:02:33 +0900

    [bin] remove broadcast.sh because ignoring escape, and add broadcast.js

---
+++ bin/broadcast.js
@@ -0,0 +1,23 @@
+#!/usr/local/bin/node
+
+var fs = require('fs');
+var spawn = require('child_process').spawn;
+
+var file = '/tmp/ws-broadcast.log';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', function( data ) {
+  fs.appendFile(file, data, function(err) {
+    if (err !== null) {
+      console.error(err);
+    }
+  });
+});
+
+var tail = spawn("tail", ["-f", file]);
+tail.stdout.setEncoding('utf8');
+tail.stdout.on('data', function(data) {
+  process.stdout.write(data);
+});
