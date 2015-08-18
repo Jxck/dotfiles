67be4623cfb1ea67f32d08fd8dfd845fff7d58d6
Jxck <block.rxckin.beats@gmail.com>
2015-08-19 01:46:30 +0900

    [install] add install-goquic.sh

---
+++ install/install-goquic.sh
@@ -0,0 +1,23 @@
+if [ `uname` = "Linux" ]; then
+  if uname -a | grep ubuntu -i >/dev/null; then
+    sudo apt-get install -y \
+      build-essential       \
+      cmake                 \
+      ninja-build
+elif [ `uname` = "Darwin" ]; then
+
+fi
+
+
+cd $HOME
+rm -rf $GOPATH/src/github.com/devsisters/goquic
+git clone https://github.com/devsisters/goquic $GOPATH/src/github.com/devsisters/goquic
+cd $GOPATH/src/github.com/devsisters/goquic
+./build_libs.sh
+go get github.com/bradfitz/http2
+go get github.com/oleiade/lane
+go get github.com/vanillahsu/go_reuseport
+
+CGO_CFLAGS="-I$GOPATH/src/github.com/devsisters/goquic/libquic/boringssl/include" \
+CGO_LDFLAGS="-L$GOPATH/src/github.com/devsisters/goquic/lib/linux_amd64" \
+go build $GOPATH/src/github.com/devsisters/goquic/example/server.go
