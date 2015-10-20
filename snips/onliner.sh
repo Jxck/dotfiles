6dd886c07bc36325b7bd6838d6182e016dfc382d
Jxck <block.rxckin.beats@gmail.com>
2015-10-20 17:45:44 +0900

    [snip] add oneliner

---
+++ snips/onliner.sh
@@ -0,0 +1,16 @@
+# SHA256
+$ echo "password2" | ruby -e "require 'digest/sha2'; p Digest::SHA256.hexdigest('$_')"
+
+# show number of row
+$ ruby -ne '$l ||= 1; $m=$l.to_s; $m="0"+$m if $m.size==1; puts "#{$m}: #{$_}"; $l+=1' test.js
+01: var i = 0;
+02: var j = 1000;
+03: function hoge() {
+
+$ grep -n '' test.js
+1:var i = 0;
+2:var j = 1000;
+3:function hoge() {
+
+# rendering markdown via github api
+wget --header=Content-Type: text/plain --post-file=readme.md https://api.github.com/markdown/raw
