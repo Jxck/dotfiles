1c6371156abdd2ad25f4b7070e867d48a34920cc
Jxck <block.rxckin.beats@gmail.com>
2016-01-08 08:01:27 +0000

    [bin] remove .gitignore from slink.sh

--- bin/slink.sh
+++ bin/slink.sh
@@ -1,4 +1,4 @@
-dotfiles=".vimrc .vim .gitconfig .gitignore .zshrc .tmux.conf .hgrc"
+dotfiles=".vimrc .vim .gitconfig .zshrc .tmux.conf .hgrc"
 for dotfile in $dotfiles
 do
   ln -s "$DOTFILES/$dotfile" $HOME
