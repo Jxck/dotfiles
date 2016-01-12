7010bc7a61423adb999b3db16692346f9e772024
Jxck <block.rxckin.beats@gmail.com>
2016-01-12 12:07:35 +0900

    [install] set $DOTFILES in install.sh

--- install/install.sh
+++ install/install.sh
@@ -12,6 +12,9 @@ echo "continue"
 ###################
 echo "alias ls='ls -la --col'" >> ~/.bashrc
 
+export DOTFILES=`dirname $0`/..
+echo DOTFILES=$DOTFILES
+
 ###################
 # distoribute
 ###################
