8a04aaf206e3482a58bf14e8a697a3c7b61a568a
Jxck <block.rxckin.beats@gmail.com>
2016-01-29 09:33:57 +0900

    [zsh] alias -g PP to panic parse

--- zsh/common.zsh
+++ zsh/common.zsh
@@ -3,6 +3,7 @@ alias -g B="1>/dev/null 2>/dev/null &" # Back
 alias -g A="2>&1" # All
 alias -g C="2>&1 | color" # Color
 alias -g H="| xxd -g 1 -c 4" # Hex
+alias -g PP="|&pp" # golang panicparse
 
 alias grep="grep --col"
 alias emacs="emacs -nw"
