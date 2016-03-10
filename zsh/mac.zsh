494077bdb9aa64a45e09fe780860f47144def924
Jxck <block.rxckin.beats@gmail.com>
2016-03-10 10:36:52 +0900

    [zsh] remove LSCOLORS/LS_COLORS

--- zsh/mac.zsh
+++ zsh/mac.zsh
@@ -1,10 +1,5 @@
 ## (Mac)
 
-# COLOR
-# ls color
-export LSCOLORS=exfxcxdxbxegedabagacad
-export LS_COLORS='di=33:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
-
 # prompt configuration
 PROMPT="%n$ "
 SPROMPT="%r is correct? [n,y,a,e]: "
