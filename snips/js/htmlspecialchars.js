5e50f26bf60f37229f60cca504adef36d3345650
Jxck <block.rxckin.beats@gmail.com>
2016-03-16 16:52:30 +0900

    [snip] add htmlspecialchars.js

---
+++ snips/js/htmlspecialchars.js
@@ -0,0 +1,8 @@
+// html special chars
+function hsc(str) {
+  return str.replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#039;');
+}
