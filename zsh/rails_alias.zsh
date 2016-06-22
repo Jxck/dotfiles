dd44c584521ad8113814677b416a95d82186c850
Jxck <block.rxckin.beats@gmail.com>
2016-06-22 20:23:16 +0900

    [zsh] remove ruby aliased to bundle exec ruby

--- zsh/rails_alias.zsh
+++ zsh/rails_alias.zsh
@@ -1,6 +1,6 @@
 alias bundle-install="bundle install --path vendor/bundle"
 alias rake="bundle exec rake"
-alias ruby="bundle exec ruby"
+#alias ruby="bundle exec ruby"
 alias rails="bundle exec rails"
 alias guard="bundle exec guard"
 alias r="rails"
