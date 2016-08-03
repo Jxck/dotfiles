991d9cd030de33e7a7b19ac3df4c76d7b50a3557
Jxck <block.rxckin.beats@gmail.com>
2016-08-03 13:30:14 +0900

    [erlang] add logger/records.hrl

---
+++ snips/erlang/rebar3/lib/rebar/priv/templates/logger.hrl
@@ -0,0 +1,6 @@
+-define(Log(A),                io:format("[~p:~p#~p] ~p~n",                [?MODULE, ?FUNCTION_NAME, ?LINE, A               ])).
+-define(Log(A, B),             io:format("[~p:~p#~p] ~p ~p~n",             [?MODULE, ?FUNCTION_NAME, ?LINE, A, B            ])).
+-define(Log(A, B, C),          io:format("[~p:~p#~p] ~p ~p ~p~n",          [?MODULE, ?FUNCTION_NAME, ?LINE, A, B, C         ])).
+-define(Log(A, B, C, D),       io:format("[~p:~p#~p] ~p ~p ~p ~p~n",       [?MODULE, ?FUNCTION_NAME, ?LINE, A, B, C, D      ])).
+-define(Log(A, B, C, D, E),    io:format("[~p:~p#~p] ~p ~p ~p ~p ~p~n",    [?MODULE, ?FUNCTION_NAME, ?LINE, A, B, C, D, E   ])).
+-define(Log(A, B, C, D, E, F), io:format("[~p:~p#~p] ~p ~p ~p ~p ~p ~p~n", [?MODULE, ?FUNCTION_NAME, ?LINE, A, B, C, D, E, F])).
