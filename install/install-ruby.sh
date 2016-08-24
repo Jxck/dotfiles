7e0f54ff1d82251de65d417f7b4bb2e0c6b0d676
jxck <block.rxckin.beats@gmail.com>
2016-08-24 05:58:44 +0000

    [install] update ruby version

--- install/install-ruby.sh
+++ install/install-ruby.sh
@@ -29,6 +29,6 @@ git clone https://github.com/sstephenson/rbenv $DEST
 mkdir -p $DEST/plugins
 git clone https://github.com/sstephenson/ruby-build $DEST/plugins/ruby-build
 
-$DEST/bin/rbenv install 2.2.3
-$DEST/bin/rbenv global 2.2
+$DEST/bin/rbenv install 2.3.1
+$DEST/bin/rbenv global 2.3
 # rbenv exec gem install bundler rails
