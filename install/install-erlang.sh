4e06c282aae01ea18c72a26617a7234608c71338
Jxck <block.rxckin.beats@gmail.com>
2017-01-17 14:47:45 +0900

    [install] support brew for install-erlang

--- install/install-erlang.sh
+++ install/install-erlang.sh
@@ -1,5 +1,10 @@
 cd /tmp
-wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
-sudo dpkg -i erlang-solutions_1.0_all.deb
-sudo apt-get update
-sudo apt-get install erlang
+if [ `uname` = "Linux" ]; then
+  wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+  sudo dpkg -i erlang-solutions_1.0_all.deb
+  sudo apt-get update
+  sudo apt-get install erlang
+elif [ `uname` = "Darwin" ]; then
+  brew update
+  brew install erlang
+fi
