f1871c5aed58d67035e4df5e96ff014147457b35
Jxck <block.rxckin.beats@gmail.com>
2017-07-11 14:10:44 +0900

    [install] update install-erlang version to 20

--- install/install-erlang.sh
+++ install/install-erlang.sh
@@ -4,8 +4,8 @@ if [ `uname` = "Linux" ]; then
   # sudo dpkg -i erlang-solutions_1.0_all.deb
   # sudo apt-get update
   # sudo apt-get install erlang
-  wget https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_19.3.6-1~ubuntu~xenial_amd64.deb
-  sudo dpkg -i esl-erlang_19.3.6-1~ubuntu~xenial_amd64.deb
+  wget https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_20.0-1~ubuntu~zesty_amd64.deb
+  sudo dpkg -i esl-erlang_20.0-1~ubuntu~zesty_amd64.deb
 elif [ `uname` = "Darwin" ]; then
   brew update
   brew install erlang
