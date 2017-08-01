9e4378631a20618c92e75cc19b5d086fc66fe817
Jxck <block.rxckin.beats@gmail.com>
2017-08-01 13:47:48 +0900

    [install] update install-otp to v20

--- install/install-otp.sh
+++ install/install-otp.sh
@@ -1,7 +1,7 @@
 cd $DOTFILES/pkg
-curl -LO http://erlang.org/download/otp_src_19.3.tar.gz
-tar zxvf otp_src_19.3.tar.gz
-cd otp_src_19.3
+curl -LO http://erlang.org/download/otp_src_20.0.tar.gz
+tar zxvf otp_src_20.0.tar.gz
+cd otp_src_20.0
 ./configure --prefix=$DOTFILES/local/otp
 make
 make install
