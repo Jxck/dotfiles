efd84872004a4cd5bfb00a83ad3b791d340ac6c7
Jxck <block.rxckin.beats@gmail.com>
2017-11-20 11:20:03 +0900

    [bin] update indent.sh

--- bin/indent.sh
+++ bin/indent.sh
@@ -1,2 +1,2 @@
-# indent files with vim gg=G
-vim -c "argdo execute 'normal! gg=G' | update" $@ -c quitall
+# indent files with vim gg=G in parallel
+echo $@ | xargs -I{} -L 1 -P 30 vim -c "argdo execute 'normal! gg=G' | update" {} -c quitall
