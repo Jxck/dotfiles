c1d933b6e4a636d792a33d7cfc499d39e11ea7ac
Jxck <block.rxckin.beats@gmail.com>
2017-12-28 11:15:21 +0900

    [install] update install-tmux2.sh

--- install/install-tmux2.sh
+++ install/install-tmux2.sh
@@ -1,8 +1,22 @@
+if [ `uname` = "Linux" ]; then
+  sudo apt-get update
+  sudo apt-get install -y \
+    automake \
+    pkg-config
+elif [ `uname` = "Darwin" ]; then
+  brew install \
+    automake \
+    pkg-config
+fi
+
 cd $DOTFILES/pkg
-wget https://github.com/tmux/tmux/releases/download/2.2/tmux-2.2.tar.gz
-tar zxvf tmux-2.2.tar.gz
-rm tmux-2.2.tar.gz
-cd tmux-2.2
+
+rm -rf $DOTFILES/pkg/tmux
+ghlatest tmux/tmux
+cd $DOTFILES/pkg/tmux
+sh autogen.sh
+
 ./configure --prefix=$DOTFILES/local/tmux
+rm -rf $DOTFILES/local/tmux
 make
 make install
