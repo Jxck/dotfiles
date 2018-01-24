dc5f0497595bb2dd221f95052fa5ca0298541de1
Jxck <block.rxckin.beats@gmail.com>
2018-01-24 20:22:42 +0900

    [bin] support mac in git-touch.sh

--- bin/git-touch.sh
+++ bin/git-touch.sh
@@ -1,6 +1,13 @@
+
 for FILE in `git ls-files`; do
   LOG=`git log --pretty=format:%ci -n1 $FILE`
-  TIME=`date -d "$LOG" +"%y%m%d%H%M.%S"`
+
+  if [ `uname` == 'Darwin' ]; then
+    TIME=`date -jf "%Y-%m-%d %H:%M:%S" "$LOG" "+%y%m%d%H%M.%S"`
+  else
+    TIME=`date -d "$LOG" +"%y%m%d%H%M.%S"`
+  fi
+
   echo "$FILE': \t'$LOG'\t'$TIME"
   touch -t $TIME $FILE
 done
