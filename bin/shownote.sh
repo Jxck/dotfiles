244e442a0a6b433b5905307d117c13d6f17e4779
Jxck <block.rxckin.beats@gmail.com>
2018-10-23 20:35:14 +0900

    [bin] fixup shownote.sh

--- bin/shownote.sh
+++ bin/shownote.sh
@@ -4,5 +4,8 @@ sed -i -e 's/^\* /- /g' $@
 sed -i -e 's/^   \* /  - /g' $@
 sed -i -e 's/^      \* /    - /g' $@
 sed -i -e 's/^         \* /      - /g' $@
+sed -i -e 's/^\(#.*\)/\n\1\n/g' $@
+sed -i -e 's/________________//g' $@
+sed -i -e 's/- \(https:\/\/.*\)/- <\1>/g' $@
 singler $@
 spacer $@
