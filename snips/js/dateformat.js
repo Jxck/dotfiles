32fc1177feedaee194c2b38bc9a7cd1b87787d59
Jxck <block.rxckin.beats@gmail.com>
2018-10-23 20:04:24 +0900

    [snip] up dateformat

--- snips/js/dateformat.js
+++ snips/js/dateformat.js
@@ -1,33 +1,16 @@
-(function(date) {
-  var zeroPadding = function(n, len) {
-    n = n.toString();
-    var l = n.length;
-    if (l < len) {
-      return '0' + zeroPadding(n, len - 1);
-    }
-    return n;
-  };
-
-  var MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  var d = date.getDate(),
-      m = MONTHS[date.getMonth()],
-      y = date.getFullYear(),
-      h = date.getHours(),
-      mi = date.getMinutes(),
-      s = date.getSeconds(),
-      z = date.getTimezoneOffset().toString();
-
-  var sign = z.charAt(0);
-      z = z.substr(1);
-
-  var day = zeroPadding(d, 2),
-      month = m,
-      year = y,
-      hour = zeroPadding(h, 2),
-      minute = zeroPadding(mi, 2),
-      second = zeroPadding(s, 2),
-      zone = zeroPadding(z, 4);
+function format(date) {
+  const day    = date.getDate()     .toString().padStart(2, 0)
+  const month  = (date.getMonth()+1).toString().padStart(2, 0)
+  const year   = date.getFullYear() .toString().padStart(2, 0)
+  const hour   = date.getHours()    .toString().padStart(2, 0)
+  const minute = date.getMinutes()  .toString().padStart(2, 0)
+  const second = date.getSeconds()  .toString().padStart(2, 0)
+  const offset = date.getTimezoneOffset()
+  const sign   = offset > 0 ? "+" : "-"
+  const zone   = Math.abs(offset/60).toString().padStart(2, 0).padEnd(4, 0)
 
   //[day/month/year:hour:minute:second zone]
-  return '[' + day + '/' + month + '/' + year + ':' + hour + ':' + minute + ':' + second + ' ' + sign + zone + ']';
-})(new Date()); // "[02/Aug/2013:22:19:08 -0540]"
+  return `[${month}/${day}/${year}:${hour}:${minute}:${second} ${sign}${zone}]`
+}
+
+console.log(format(new Date())); // "[02/Aug/2013:22:19:08 -0540]"
