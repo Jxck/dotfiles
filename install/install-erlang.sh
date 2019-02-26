db5ad13156a6844a19a72a587761429d40f9cb2b
Jxck <block.rxckin.beats@gmail.com>
2019-02-26 18:05:07 +0900

    [install] update install-erlang version

--- install/install-erlang.sh
+++ install/install-erlang.sh
@@ -4,8 +4,8 @@ if [ `uname` = "Linux" ]; then
   # sudo dpkg -i erlang-solutions_1.0_all.deb
   # sudo apt update
   # sudo apt install erlang
-  wget https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_21.1-1~ubuntu~bionic_amd64.deb
-  sudo dpkg -i esl-erlang_21.1-1~ubuntu~bionic_amd64.deb
+  wget https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_21.2.6-1~ubuntu~cosmic_amd64.deb
+  sudo dpkg -i esl-erlang_21.2.6-1~ubuntu~cosmic_amd64.deb
 elif [ `uname` = "Darwin" ]; then
   brew update
   brew install erlang
