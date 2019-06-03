0af4e15a9057a64ba0d0c6637b8330501ae10a71
Jxck <block.rxckin.beats@gmail.com>
2019-06-03 12:55:59 +0900

    [bin] up japanese-name

--- bin/japanese-name.rb
+++ bin/japanese-name.rb
@@ -3,29 +3,21 @@
 names = DATA.read.downcase.split("\n")
 
 result = names.select{|name|
-    # これらの文字を含まない
-    !(
-        name.include?("l") ||
-        name.include?("q") ||
-        name.include?("v") ||
-        name.include?("x") ||
-        name.include?(".") ||
-        name.include?(",")
-    )
+  # これらの文字を含まない
+  !(name.match?(/(l|q|v|x|\.|,)/))
 }.map{|name|
-    # 名前と名字に分割
-    name.split(" ")
+  # 名前と名字に分割
+  name.split(" ")
 }.select{|name|
-    # ミドルネームなどが無い
-    name.size <= 2
+  # ミドルネームなどが無い
+  name.size <= 2
 }.select{|name|
-    # 名前/名字の最後は n か母音で終わる
-    name.first.end_with?("a", "i", "u", "e", "o", "n") && name.last.end_with?("a", "i", "u", "e", "o", "n")
+  # 名前/名字の最後は n か母音で終わる
+  name.first.end_with?("a", "i", "u", "e", "o", "n") && name.last.end_with?("a", "i", "u", "e", "o", "n")
 }.map{|name|
-    name.join(" ")
+  name.join(" ")
 }
 
-p result
+puts result
 
-# example https://lists.w3.org/Archives/Public/www-style/2017Dec/0004.html
 __END__
