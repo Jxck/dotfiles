53b44a1261101644db3fb6ef3cf8c2a90422c521
Jxck <block.rxckin.beats@gmail.com>
2019-08-14 23:33:47 +0900

    [bin] up git-touch

--- bin/git-touch.sh
+++ bin/git-touch.sh
@@ -1,13 +1,15 @@
+#!/usr/bin/zsh
+
 # revert file access time to git log
-for FILE in `git ls-files`; do
+for FILE in `git ls-files | grep '.md$'`; do
   LOG=`git log --pretty=format:%ci -n1 $FILE`
 
-  if [ `uname` == 'Darwin' ]; then
+  if [ `uname` = 'Darwin' ]; then
     TIME=`date -jf "%Y-%m-%d %H:%M:%S" "$LOG" "+%y%m%d%H%M.%S"`
   else
     TIME=`date -d "$LOG" +"%y%m%d%H%M.%S"`
   fi
 
-  echo "$FILE': \t'$LOG'\t'$TIME"
+  echo "$LOG\t$TIME\t$FILE"
   touch -t $TIME $FILE
 done
