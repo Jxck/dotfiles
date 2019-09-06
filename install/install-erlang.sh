ec9d583fe404a3258a28ca9948c0c8bd36747245
Jxck <block.rxckin.beats@gmail.com>
2019-09-06 14:16:42 +0900

    [install] up erlang

--- install/install-erlang.sh
+++ install/install-erlang.sh
@@ -4,8 +4,8 @@ if [ `uname` = "Linux" ]; then
   # sudo dpkg -i erlang-solutions_1.0_all.deb
   # sudo apt update
   # sudo apt install erlang
-  wget https://packages.erlang-solutions.com/erlang/debian/pool/esl-erlang_22.0.1-1~ubuntu~cosmic_amd64.deb
-  sudo dpkg -i esl-erlang_22.0.1-1~ubuntu~cosmic_amd64.deb
+  wget https://packages.erlang-solutions.com/erlang/debian/pool/esl-erlang_22.0.7-1~ubuntu~disco_amd64.deb
+  sudo dpkg -i esl-erlang_22.0.7-1~ubuntu~disco_amd64.deb
 elif [ `uname` = "Darwin" ]; then
   brew update
   brew install erlang
