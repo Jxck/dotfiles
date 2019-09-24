12d8bcd2f5988463af5e86ce9f9f8b7d31798178
Jxck <block.rxckin.beats@gmail.com>
2019-09-24 16:36:36 +0900

    [keys] copy.sh

---
+++ keys/copy.sh
@@ -0,0 +1,5 @@
+rm *pem
+scp jxck@jxck.io:/etc/letsencrypt/archive/localhost.jxck.io/cert*.pem ./cert.pem
+scp jxck@jxck.io:/etc/letsencrypt/archive/localhost.jxck.io/chain*.pem ./chain.pem
+scp jxck@jxck.io:/etc/letsencrypt/archive/localhost.jxck.io/fullchain*.pem ./fullchain.pem
+scp jxck@jxck.io:/etc/letsencrypt/archive/localhost.jxck.io/privkey*.pem ./privkey.pem
