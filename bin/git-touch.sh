a9df59cb22c40302793ea1c1d041849f0552e56c
Jxck <block.rxckin.beats@gmail.com>
2019-10-22 16:15:29 +0900

    [bin] update git-touch with gdate in Darwin

--- bin/git-touch.sh
+++ bin/git-touch.sh
@@ -5,9 +5,9 @@ for FILE in `git ls-files | grep '.md$'`; do
   LOG=`git log --pretty=format:%ci -n1 $FILE`
 
   if [ `uname` = 'Darwin' ]; then
-    TIME=`date -jf "%Y-%m-%d %H:%M:%S" "$LOG" "+%y%m%d%H%M.%S"`
+    TIME=`gdate -d "$LOG" +"%y%m%d%H%M.%S"`
   else
-    TIME=`date -d "$LOG" +"%y%m%d%H%M.%S"`
+    TIME=`date  -d "$LOG" +"%y%m%d%H%M.%S"`
   fi
 
   echo "$LOG\t$TIME\t$FILE"
