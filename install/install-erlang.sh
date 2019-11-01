c95749bc5e6cdafd1d36ac249f86070f5feb01f3
Jxck <block.rxckin.beats@gmail.com>
2019-11-01 13:58:03 +0900

    [install] update install-erlang

--- install/install-erlang.sh
+++ install/install-erlang.sh
@@ -4,6 +4,9 @@ if [ `uname` = "Linux" ]; then
   # sudo dpkg -i erlang-solutions_1.0_all.deb
   # sudo apt update
   # sudo apt install erlang
+
+  sudo apt install libtinfo5 libncurses5 libwxbase3.0-0v5 libwxgtk3.0-0v5 libsctp1
+
   VERSION=22.0.7
   wget https://packages.erlang-solutions.com/erlang/debian/pool/esl-erlang_$VERSION-1~ubuntu~disco_amd64.deb
   sudo dpkg -i esl-erlang_$VERSION-1~ubuntu~disco_amd64.deb
