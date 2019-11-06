d5eb131378bc1fdf2ce713fd95a1fe3d9eb7c022
Jxck <block.rxckin.beats@gmail.com>
2019-11-06 14:29:54 +0900

    [install] install certbot

--- install/install-certbot.sh
+++ install/install-certbot.sh
@@ -1,15 +1,14 @@
-DEST=$DOTFILES/local/certbot-auto
-rm -rf $DEST
-mkdir $DEST
-cd $DEST
-wget https://dl.eff.org/certbot-auto
-chmod a+x ./certbot-auto
-./certbot-auto --help
+# DEST=$DOTFILES/local/certbot-auto
+# rm -rf $DEST
+# mkdir $DEST
+# cd $DEST
+# wget https://dl.eff.org/certbot-auto
+# chmod a+x ./certbot-auto
+# ./certbot-auto --help
 
-
-# sudo apt update
-# sudo apt install software-properties-common
-# sudo add-apt-repository universe
-# sudo add-apt-repository ppa:certbot/certbot
-# sudo apt update
-# sudo apt install certbot
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt update
+sudo apt install certbot
