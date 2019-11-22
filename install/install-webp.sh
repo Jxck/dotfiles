cc1ac51ff28920fff1e7eff8933e2ddfe975734e
Jxck <block.rxckin.beats@gmail.com>
2019-11-22 15:50:30 +0900

    [install] add install webp

---
+++ install/install-webp.sh
@@ -0,0 +1,18 @@
+\rm -rf $DOTFILES/pkg/webp
+mkdir $DOTFILES/pkg/webp
+cd $DOTFILES/pkg/webp
+
+URL=https://storage.googleapis.com/downloads.webmproject.org/releases/webp/
+
+
+if [ `uname` = "Linux" ]; then
+  VERSION=libwebp-1.0.3-mac-10.14
+elif [ `uname` = "Darwin" ]; then
+  VERSION=libwebp-1.0.3-linux-x86-64
+fi
+
+wget $URL$VERSION.tar.gz
+unpack $VERSION.tar.gz
+mv $VERSION/bin .
+\rm -rf $VERSION
+\rm -rf $VERSION.tar.gz
