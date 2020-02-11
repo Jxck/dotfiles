36d5de49d24e5690de3dbf2b04cd97a5fb2c2146
Jxck <block.rxckin.beats@gmail.com>
2020-02-11 14:07:04 +0900

    [install] update install-xkeysnail

--- install/install-xkeysnail.sh
+++ install/install-xkeysnail.sh
@@ -1,6 +1,6 @@
 # install
-# sudo apt install python3-pip
-# sudo pip3 install xkeysnail
+sudo apt install python3-pip
+sudo pip3 install xkeysnail
 
 # uinput グループを作り自分を追加
 sudo groupadd uinput
@@ -10,15 +10,15 @@ sudo usermod -aG input,uinput yusuke
 sudo ln -sf $DOTFILES/conf/xkeysnail/input.rules /etc/udev/rules.d
 sudo ln -sf $DOTFILES/conf/xkeysnail/uinput.rules /etc/udev/rules.d
 
+# コンフィグがローカルにあるとパスが面倒なので /etc/xkeysnail にコピー
+sudo mkdir /etc/xkeysnail
+sudo ln -sf $DOTFILES/conf/xkeysnail/xkeysnail.py /etc/xkeysnail
+
 # keysnail  への sudo に password 省略
 echo "$USER ALL=(ALL) ALL, NOPASSWD: /usr/local/bin/xkeysnail" > /tmp/sudo-xkeysnail
 chmod 0440 /tmp/sudo-xkeysnail
 sudo chown root:root /tmp/sudo-xkeysnail
 sudo mv /tmp/sudo-xkeysnail /etc/sudoers.d
-
-# コンフィグがローカルにあるとパスが面倒なので /etc/xkeysnail にコピー
-sudo mkdir /etc/xkeysnail
-sudo ln -sf $DOTFILES/conf/xkeysnail/xkeysnail.py /etc/xkeysnail
 
 # link systemd
 ln -sf $DOTFILES/systemd/xkeysnail.service ~/.config/systemd/user
