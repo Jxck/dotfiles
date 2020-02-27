1dd84425c6935b55d9c1f5ccb7f4af09509f0530
Jxck <block.rxckin.beats@gmail.com>
2020-02-27 16:20:04 +0900

    [install] update erlang

--- install/install-erlang.sh
+++ install/install-erlang.sh
@@ -7,7 +7,7 @@ if [ `uname` = "Linux" ]; then
 
   sudo apt install libtinfo5 libncurses5 libwxbase3.0-0v5 libwxgtk3.0-0v5 libsctp1
 
-  VERSION=22.2.6
+  VERSION=22.2.7
   wget https://packages.erlang-solutions.com/erlang/debian/pool/esl-erlang_$VERSION-1~ubuntu~disco_amd64.deb
   sudo dpkg -i esl-erlang_$VERSION-1~ubuntu~disco_amd64.deb
 elif [ `uname` = "Darwin" ]; then
