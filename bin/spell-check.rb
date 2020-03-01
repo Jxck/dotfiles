8cb407db3fc64f78c34551fe931873592675db73
Jxck <block.rxckin.beats@gmail.com>
2020-03-01 16:50:51 +0900

    [bin] update spell-check

--- bin/spell-check.rb
+++ bin/spell-check.rb
@@ -9,11 +9,18 @@ def main(paths)
       .reject{|w|
         w == "" || w.match(/^[0-9]*$/)
       }
+      .map(&:downcase)
   }.flatten
    .sort
    .group_by(&:itself)
-   .map{|k, v| "#{k}, #{v.count}"}
-   .sort_by(&:downcase)
+   .sort
+   .map{|k, v|
+      if v.count <= 2 then
+        "#{k}: \e[0;101m#{v.count}\e[0m"
+      else
+        "#{k}, #{v.count}"
+      end
+    }
    .each{|s| puts(s)}
 end
 
