735a8e27a1066fd09a1b3f0cd61e89e103babeff
Jxck <block.rxckin.beats@gmail.com>
2020-05-28 02:53:17 +0000

    [install] install-git-delta

---
+++ install/install-git-delta.sh
@@ -0,0 +1,7 @@
+if [ `uname` = "Darwin" ]; then
+  echo "install via brew"
+  exit 0
+fi
+
+curl -L https://github.com/dandavison/delta/releases/download/0.1.1/git-delta_0.1.1_amd64.deb -o "/tmp/git-delta_0.1.1_amd64.deb"
+sudo dpkg -i /tmp/git-delta_0.1.1_amd64.deb
