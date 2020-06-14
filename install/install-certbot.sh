b4faeded94264351a9cd6431ec31da703ee1c0c4
Jxck <block.rxckin.beats@gmail.com>
2020-06-14 18:26:30 +0900

    [install] up install-certbot

--- install/install-certbot.sh
+++ install/install-certbot.sh
@@ -9,6 +9,5 @@
 sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository universe
-sudo add-apt-repository ppa:certbot/certbot
 sudo apt update
 sudo apt install certbot
