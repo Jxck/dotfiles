3515e3e5e1f98e23573acab4ede233fbd7cc4ff3
Jxck <block.rxckin.beats@gmail.com>
2020-07-13 16:19:15 +0900

    [keys] up sxg.sh

--- keys/sxg/sxg.sh
+++ keys/sxg/sxg.sh
@@ -1,5 +1,8 @@
 # https://www.digicert.com/kb/ecc-csr-creation-ssl-installation-apache.htm
 # CAA: 0 issue "digicert.com; cansignhttpexchanges=yes"
-openssl ecparam -out sxg.key -name prime256v1 -genkey
-openssl req -new -key sxg.key -sha256 -out sxg.csr
-openssl req -text -in sxg.csr
+## openssl ecparam -out  labs.jxck.io.sxg.key -name prime256v1 -genkey
+## openssl req -new -key labs.jxck.io.sxg.key -sha256 -out labs.jxck.io.sxg.csr -subj '/CN=labs.jxck.io'
+## openssl req -text -in labs.jxck.io.sxg.csr
+##
+## chmod -w labs.jxck.io.*
+## sudo chown root:root labs.jxck.io.*
