4c837e4919864d062bd65285dcd9ef95fbce5c95
Jxck <block.rxckin.beats@gmail.com>
2020-07-13 15:43:14 +0900

    [keys] up sxg.sh

--- keys/sxg/sxg.sh
+++ keys/sxg/sxg.sh
@@ -1,3 +1,5 @@
+# https://www.digicert.com/kb/ecc-csr-creation-ssl-installation-apache.htm
+# CAA: 0 issue "digicert.com; cansignhttpexchanges=yes"
 openssl ecparam -out sxg.key -name prime256v1 -genkey
 openssl req -new -key sxg.key -sha256 -out sxg.csr
 openssl req -text -in sxg.csr
