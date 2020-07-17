6272e9f10fcf7bf1cb778e0d7ef0411b0064e6fd
Jxck <block.rxckin.beats@gmail.com>
2020-07-18 00:37:00 +0900

    [key] up sxg.sh

--- keys/sxg/sxg.sh
+++ keys/sxg/sxg.sh
@@ -3,6 +3,7 @@
 ## openssl ecparam -out  labs.jxck.io.sxg.key -name prime256v1 -genkey
 ## openssl req -new -key labs.jxck.io.sxg.key -sha256 -out labs.jxck.io.sxg.csr -subj '/CN=labs.jxck.io'
 ## openssl req -text -in labs.jxck.io.sxg.csr
+openssl req -noout -in labs.jxck.io.sxg.csr -pubkey -out labs.jxck.io.sxg.pub
 ##
 ## chmod -w labs.jxck.io.*
 ## sudo chown root:root labs.jxck.io.*
