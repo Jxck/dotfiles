876d73dc3a419a567d03a4e9b2f69f266ee7a495
jxck <block.rxckin.beats@gmail.com>
2020-08-15 14:42:32 +0900

    [bin] up url.js

--- bin/url.js
+++ bin/url.js
@@ -1,23 +1,26 @@
 #!/usr/bin/env osascript -l JavaScript
 
 function run(arg) {
-  console.log("run with", arg)
-
+  arg = arg.toString()
   let browser = ''
   switch(arg) {
     case 'chrome':
       browser = 'Google Chrome'
+      break;
     case 'edge':
       browser = "Microsoft Edge"
+      break;
     default:
       browser = 'Google Chrome'
+      break;
   }
 
   Application(browser).windows().forEach((window) => {
+    console.log('\n\n')
     window.tabs().forEach((tab) => {
       const url  = tab.url()
       const name = tab.name()
-      console.log(name + "\r\n\t" + url)
+      console.log(name + "\n" + url)
     })
   })
 }
