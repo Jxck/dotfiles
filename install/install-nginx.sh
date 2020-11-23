ad8aa45566f4c58c28b7f73ddce36892cdd86180
Jxck <block.rxckin.beats@gmail.com>
2020-11-23 09:45:48 +0000

    [install] up nginx

--- install/install-nginx.sh
+++ install/install-nginx.sh
@@ -3,9 +3,9 @@ DEST=$DOTFILES/pkg/nginx
 rm -rf $DEST
 
 cd /tmp
-wget http://nginx.org/download/nginx-1.10.1.tar.gz
-tar zxvf /tmp/nginx-1.10.1.tar.gz
-mv /tmp/nginx-1.10.1 $DEST
+wget http://nginx.org/download/nginx-1.18.0.tar.gz
+tar zxvf /tmp/nginx-1.18.0.tar.gz
+mv /tmp/nginx-1.18.0 $DEST
 cd $DEST
 
 ./configure --prefix=$DOTFILES/local/nginx
