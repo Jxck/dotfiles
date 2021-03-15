a13ed257cb6e0bc1bfc220529dddde1d95b4f2df
Jxck <block.rxckin.beats@gmail.com>
2021-03-15 16:06:22 +0900

    [bin] up etag.rb

--- bin/etag.rb
+++ bin/etag.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
+require "pathname"
 
 def etag(path)
-  mtime = File.mtime(path)
-  size  = File.size(path)
-  sprintf("%08x-%x", mtime, size)
+  tag = sprintf("%08x-%x", File.mtime(path), File.size(path))
+  "#{File.basename(path, ".*")}.#{tag}#{File.extname(path)}"
 end
 
 if __FILE__ == $0
