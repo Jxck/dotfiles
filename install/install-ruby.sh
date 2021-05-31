00c885ebc364aa2ae0e5afe5bcbbb5b225e8bba1
jxck <block.rxckin.beats@gmail.com>
2021-05-31 17:19:18 +0900

    [install] update ruby to 3.0.0

--- install/install-ruby.sh
+++ install/install-ruby.sh
@@ -24,6 +24,6 @@ git clone --depth 1 https://github.com/sstephenson/ruby-build $DEST/plugins/ruby
 mkdir -p $DEST/plugins
 git clone --depth 1 https://github.com/rkh/rbenv-update.git $DEST/plugins/rbenv-update
 
-$DEST/bin/rbenv install 2.7.0
-$DEST/bin/rbenv global 2.7.0
-# rbenv exec gem install bundler rails
+$DEST/bin/rbenv install 3.0.0
+$DEST/bin/rbenv global 3.0.0
+# rbenv exec gem install -N bundler rails
