9b8ceaacb875e498201b93a02f6bd220738cd8fa
Jxck <block.rxckin.beats@gmail.com>
2021-12-18 02:14:24 +0900

    [keys] up copy.sh

--- keys/local-copy.sh
+++ keys/local-copy.sh
@@ -1,6 +1,13 @@
 rm -rf *pm
-sudo cp /etc/letsencrypt/live/localhost.jxck.io/cert.pem ./cert.pem
-sudo cp /etc/letsencrypt/live/localhost.jxck.io/chain.pem ./chain.pem
-sudo cp /etc/letsencrypt/live/localhost.jxck.io/fullchain.pem ./fullchain.pem
-sudo cp /etc/letsencrypt/live/localhost.jxck.io/privkey.pem ./privkey.pem
+
+sudo cp /etc/letsencrypt/live/localhost.jxck.io/cert.pem      ./localhost.jxck.io.cert.pem
+sudo cp /etc/letsencrypt/live/localhost.jxck.io/chain.pem     ./localhost.jxck.io.chain.pem
+sudo cp /etc/letsencrypt/live/localhost.jxck.io/fullchain.pem ./localhost.jxck.io.fullchain.pem
+sudo cp /etc/letsencrypt/live/localhost.jxck.io/privkey.pem   ./localhost.jxck.io.privkey.pem
+
+sudo cp /etc/letsencrypt/live/jxck.dev/cert.pem      ./jxck.dev.cert.pem
+sudo cp /etc/letsencrypt/live/jxck.dev/chain.pem     ./jxck.dev.chain.pem
+sudo cp /etc/letsencrypt/live/jxck.dev/fullchain.pem ./jxck.dev.fullchain.pem
+sudo cp /etc/letsencrypt/live/jxck.dev/privkey.pem   ./jxck.dev.privkey.pem
+
 sudo chown jxck:jxck ./*pem
--- keys/remote-copy.sh
+++ keys/remote-copy.sh
@@ -1,5 +1,11 @@
 rm *pem
-scp jxck@jxck.io:/home/jxck/dotfiles/keys/cert.pem ./cert.pem
-scp jxck@jxck.io:/home/jxck/dotfiles/keys/chain.pem ./chain.pem
-scp jxck@jxck.io:/home/jxck/dotfiles/keys/fullchain.pem ./fullchain.pem
-scp jxck@jxck.io:/home/jxck/dotfiles/keys/privkey.pem ./privkey.pem
+
+scp jxck@jxck.io:/home/jxck/dotfiles/keys/localhost.jxck.io.cert.pem      ./localhost.jxck.io.cert.pem
+scp jxck@jxck.io:/home/jxck/dotfiles/keys/localhost.jxck.io.chain.pem     ./localhost.jxck.io.chain.pem
+scp jxck@jxck.io:/home/jxck/dotfiles/keys/localhost.jxck.io.fullchain.pem ./localhost.jxck.io.fullchain.pem
+scp jxck@jxck.io:/home/jxck/dotfiles/keys/localhost.jxck.io.privkey.pem   ./localhost.jxck.io.privkey.pem
+
+scp jxck@jxck.io:/home/jxck/dotfiles/keys/jxck.dev.cert.pem               ./jxck.dev.cert.pem
+scp jxck@jxck.io:/home/jxck/dotfiles/keys/jxck.dev.chain.pem              ./jxck.dev.chain.pem
+scp jxck@jxck.io:/home/jxck/dotfiles/keys/jxck.dev.fullchain.pem          ./jxck.dev.fullchain.pem
+scp jxck@jxck.io:/home/jxck/dotfiles/keys/jxck.dev.privkey.pem            ./jxck.dev.privkey.pem
