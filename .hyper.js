b8d1c2c07910fb2f771f6f52ff0529de747a839b
Jxck <jxck@jxck.io>
2023-10-28 16:04:32 +0900

    [hyper] install hyper-material-theme

--- .hyper.js
+++ .hyper.js
@@ -137,7 +137,7 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: [],
+    plugins: ["hyper-material-theme"],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
