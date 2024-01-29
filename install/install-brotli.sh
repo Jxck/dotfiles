6e5ca598a141ade4328c5e34eed7eb9267ced472
Jxck <block.rxckin.beats@gmail.com>
2024-01-29 19:01:45 +0900

    [install] update install-brotli

--- install/install-brotli.sh
+++ install/install-brotli.sh
@@ -8,7 +8,11 @@ fi
 cd $DOTFILES/pkg
 rm -rf $DOTFILES/pkg/brotli
 rm -rf $DOTFILES/local/brotli
-ghlatest google/brotli
+gh release download -R google/brotli --archive tar.gz --output brotli.tar.gz
+result=`unpack brotli.tar.gz`
+echo $result
+version=`echo $result | head -n2 | tail -n1`
+mv $version brotli
 cd $DOTFILES/pkg/brotli
 mkdir $DOTFILES/local/brotli
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DOTFILES/local/brotli
