21e43267dd773c81598578b0ef15ba666efb2ef1
Jxck <block.rxckin.beats@gmail.com>
2024-02-13 02:02:53 +0900

    [install] tmux to brew

--- install/install-tmux.sh
+++ install/install-tmux.sh
@@ -1,29 +1,27 @@
 #!/usr/bin/env zsh
 
-#if [[ `uname` == "Darwin" ]]; then
-#  echo "install via brew"
-#  exit 0
+echo "install via brew"
+exit 0
+
+#if [[ `uname` == "Linux" ]]; then
+#  sudo apt update
+#  sudo apt install -y \
+#    automake \
+#    pkg-config
+#elif [[ `uname` == "Darwin" ]]; then
+#  brew install \
+#    automake \
+#    pkg-config
 #fi
-
-if [[ `uname` == "Linux" ]]; then
-  sudo apt update
-  sudo apt install -y \
-    automake \
-    pkg-config
-elif [[ `uname` == "Darwin" ]]; then
-  brew install \
-    automake \
-    pkg-config
-fi
-
-rm -rf $DOTFILES/pkg/tmux
-rm -rf $DOTFILES/local/tmux
-
-cd $DOTFILES/pkg
-gh release download -R tmux/tmux 3.2a
-unpack tmux-3.2a.tar.gz
-cd $DOTFILES/pkg/tmux-3.2a/tmux-3.2a
-
-./configure --prefix=$DOTFILES/local/tmux
-make
-make install
+#
+#rm -rf $DOTFILES/pkg/tmux
+#rm -rf $DOTFILES/local/tmux
+#
+#cd $DOTFILES/pkg
+#gh release download -R tmux/tmux 3.2a
+#unpack tmux-3.2a.tar.gz
+#cd $DOTFILES/pkg/tmux-3.2a/tmux-3.2a
+#
+#./configure --prefix=$DOTFILES/local/tmux
+#make
+#make install
