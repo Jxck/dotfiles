83cb756cb725aa7c01d4849c82ec4032cb3db6ad
Jxck <jxck@jxck.io>
2024-08-01 21:02:28 +0900

    [bin] up chromium-update.sh

--- bin/chromium-update.sh
+++ bin/chromium-update.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env zsh
 
+echo ========================
+echo git rebase-update
+echo ========================
 git rebase-update
+
+
+echo ========================
+echo gclient sync
+echo ========================
 gclient sync
+
+
+echo ========================
+echo git rebase HEAD main
+echo ========================
 git rebase HEAD main
+
+
+echo ========================
+echo autoninja -C out/Default blink_tests all_blink content_shell
+echo ========================
 autoninja -C out/Default blink_tests all_blink content_shell
