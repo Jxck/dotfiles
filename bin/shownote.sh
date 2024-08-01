3f7692e3479c0a885f00753821cbb1b3b71d3e4e
Jxck <jxck@jxck.io>
2024-08-01 20:06:27 +0900

    [bin] up shownote.sh

--- bin/shownote.sh
+++ bin/shownote.sh
@@ -1,10 +1,3 @@
-nkf --oc=UTF-8N -Lu -d --overwrite $@
-sed -i -e 's/ *$//g' $@
-sed -i -e 's/^\* /- /g' $@
-sed -i -e 's/^   \* /  - /g' $@
-sed -i -e 's/^      \* /    - /g' $@
-sed -i -e 's/^         \* /      - /g' $@
-sed -i -e 's/^\(#.*\)/\n\1\n/g' $@
-sed -i -e 's/________________//g' $@
+shownote.rb $@
 singler $@
 spacer $@
