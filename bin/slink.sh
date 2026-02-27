c13cafbdb3e667704a8bae40bd889469c820c068
Jxck <jxck@jxck.io>
2026-02-27 16:49:00 +0900

    [bin] fixup slink.sh

--- bin/slink.sh
+++ bin/slink.sh
@@ -1,16 +1,36 @@
 dotfiles="
-.gemrc
-.gitattributes
-.gitconfig
-.gitignore_global
-.hyper.js
-.npmrc
-.tmux.conf
-.vim
-.vimrc
-.zprofile
-.zshrc
+  .gemrc
+  .gitattributes
+  .gitconfig
+  .gitignore_global
+  .hyper.js
+  .npmrc
+  .tmux.conf
+  .vim
+  .vimrc
+  .zprofile
+  .zshrc
 "
+
+for dotfile in $dotfiles
+do
+  \rm -rf $HOME/$dotfile
+  ln -s "$HOME/dotfiles/$dotfile" "$HOME/$dotfile"
+done
+
+# .claude/ と .codex/ の中身を ~/. 以下に slink
+for dir in .claude .codex; do
+  src="$HOME/dotfiles/$dir"
+  dst="$HOME/$dir"
+  if [[ -d "$src" ]]; then
+    mkdir -p "$dst"
+    for file in "$src"/*; do
+      name=$(basename "$file")
+      \rm -rf "$dst/$name"
+      ln -s "$file" "$dst/$name"
+    done
+  fi
+done
 
 cargo_config="$HOME/dotfiles/.cargo/config.toml"
 if [[ -f "$cargo_config" ]]; then
@@ -18,9 +38,3 @@ if [[ -f "$cargo_config" ]]; then
   \rm -rf $HOME/.cargo/config.toml
   ln -s "$cargo_config" $HOME/.cargo/config.toml
 fi
-
-for dotfile in $dotfiles
-do
-  \rm -rf $HOME/$dotfile
-  ln -s "$HOME/dotfiles/$dotfile" $HOME
-done
