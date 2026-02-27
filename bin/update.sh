f99e7e4753471abd7cacdaa727672a2187b18dba
Jxck <jxck@jxck.io>
2026-02-27 14:33:07 +0900

    [bin] cleanup update.sh

--- bin/update.sh
+++ bin/update.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
-if [[ `uname` == "Linux" ]]; then
+if [[ $(uname) == "Linux" ]]; then
   sudo apt update -y
   sudo apt upgrade -y
   sudo apt autoremove -y
@@ -10,24 +10,34 @@ if [[ `uname` == "Linux" ]]; then
   fwupdmgr get-upgrades
 fi
 
+# brew (mise 自体も brew で管理しているので先に実行)
+echo "=== brew ==="
 brew update
 brew upgrade
 
+# mise (brew の後に実行)
+echo "=== mise ==="
 mise completion zsh > $DOTFILES/zsh/mise.completion.zsh
-mise uninstall -a
+mise upgrade
 mise prune -y
-mise install
 
+# git fetch を並行実行
 if [[ -d $DEV/jxck.io ]]; then
-  echo "fetch jxck.io"
-  cd $DEV/jxck.io
-  git f
-  npm ci
+  (
+    echo "=== fetch jxck.io ==="
+    cd $DEV/jxck.io
+    git f
+    npm ci
+  ) &
 fi
 
 if [[ -d $DOTFILES ]]; then
-  echo "fetch dotfiles"
-  cd $DOTFILES
-  git f
-  npm ci
+  (
+    echo "=== fetch dotfiles ==="
+    cd $DOTFILES
+    git f
+  ) &
 fi
+
+wait
+echo "=== done ==="
