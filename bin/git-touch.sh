353a1473b9870204203866bc5f1acd3e2bb773e5
Jxck <jxck@jxck.io>
2026-03-09 23:28:50 +0900

    [bin] parallel git-touch.sh

--- bin/git-touch.sh
+++ bin/git-touch.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env zsh
 
 # revert file access time to git log
-for FILE in `git ls-files | grep -e '.md$' -e '.js$' -e '.css$' -e '.svg$' -e '.png$' -e '.jpeg$' -e '.webp$' `; do
-  LOG=`git log --pretty=format:%ci -n1 $FILE`
-
-  if [[ `uname` == "Darwin" ]]; then
-    TIME=`gdate -d "$LOG" +"%y%m%d%H%M.%S"`
+touch_file() {
+  FILE=$1
+  LOG=$(git log --pretty=format:%ci -n1 $FILE)
+  if [[ $(uname) == "Darwin" ]]; then
+    TIME=$(gdate -d "$LOG" +"%y%m%d%H%M.%S")
   else
-    TIME=`date  -d "$LOG" +"%y%m%d%H%M.%S"`
+    TIME=$(date  -d "$LOG" +"%y%m%d%H%M.%S")
   fi
-
   echo "$LOG\t$TIME\t$FILE"
   touch -t $TIME $FILE
-done
+}
+export -f touch_file
+
+git ls-files | grep -e '\.md$' -e '\.js$' -e '\.css$' -e '\.svg$' -e '\.png$' -e '\.jpeg$' -e '\.webp$' \
+  | xargs -P4 -I{} zsh -c 'touch_file "$@"' _ {}
