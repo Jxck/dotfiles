35d10de24f61cacfbdd07f8ded74065faa79662c
Jxck <jxck@jxck.io>
2026-03-10 00:04:19 +0900

    [bin] up git-touch

--- bin/git-touch.sh
+++ bin/git-touch.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env zsh
 
 # revert file access time to git log
-touch_file() {
-  FILE=$1
-  LOG=$(git log --pretty=format:%ci -n1 $FILE)
-  if [[ $(uname) == "Darwin" ]]; then
-    TIME=$(gdate -d "$LOG" +"%y%m%d%H%M.%S")
-  else
-    TIME=$(date  -d "$LOG" +"%y%m%d%H%M.%S")
-  fi
-  echo "$LOG\t$TIME\t$FILE"
-  touch -t $TIME $FILE
-}
-export -f touch_file
+if [[ $(uname) == "Darwin" ]]; then
+  DATECMD=gdate
+else
+  DATECMD=date
+fi
 
-git ls-files | grep -e '\.md$' -e '\.js$' -e '\.css$' -e '\.svg$' -e '\.png$' -e '\.jpeg$' -e '\.webp$' \
-  | xargs -P4 -I{} zsh -c 'touch_file "$@"' _ {}
+git ls-files -- ${1:-.} | grep -e '\.md$' -e '\.js$' -e '\.css$' -e '\.svg$' -e '\.png$' -e '\.jpeg$' -e '\.webp$' \
+  | xargs -P4 -I{} zsh -c '
+    FILE=$1
+    DATECMD=$2
+    LOG=$(git log --pretty=format:%ci -n1 "$FILE")
+    TIME=$($DATECMD -d "$LOG" +"%y%m%d%H%M.%S")
+    echo "$LOG\t$TIME\t$FILE"
+    touch -t $TIME "$FILE"
+  ' _ {} $DATECMD
