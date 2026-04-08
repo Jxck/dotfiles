4675072661db5e9f17d8991fb795fb1786a6e047
Jxck <jxck@jxck.io>
2026-04-08 14:11:13 +0900

    [install] up dictionary-generator

--- install/install-dictionary-generator.sh
+++ install/install-dictionary-generator.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env zsh
 
-\rm -rf $DOTFILES/pkg/dictionary-generator
-mkdir $DOTFILES/pkg/dictionary-generator
-cd $DOTFILES/pkg/dictionary-generator
+PKG=$DOTFILES/pkg/dictionary-generator
+\rm -rf $PKG
+mkdir -p $PKG
+cd $PKG
 
+# brotli
 git clone --depth 1 https://github.com/google/brotli.git
-cd brotli
-bazel build brotli
-cd research
-bazel build dictionary_generator
+
+# esaxx (header-only, required by deorummolae)
+git clone --depth 1 https://github.com/hillbig/esaxx.git
+
+# build
+DIVSUFSORT=$(brew --prefix libdivsufsort)
+cd brotli/research
+g++ -O2 -std=c++11 \
+  -I${DIVSUFSORT}/include \
+  -I${PKG}/esaxx \
+  -L${DIVSUFSORT}/lib \
+  dictionary_generator.cc sieve.cc durchschlag.cc deorummolae.cc \
+  -ldivsufsort \
+  -Wl,-rpath,${DIVSUFSORT}/lib \
+  -o ${PKG}/dictionary_generator
