6458a865f89cc95a736a54d2cbdacf8ca4ce24b0
Jxck <jxck@jxck.io>
2026-04-28 14:56:13 +0900

    [install] install-git-lfs-transfer

---
+++ install/install-git-lfs-transfer.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+# https://github.com/charmbracelet/git-lfs-transfer
+# Server-side implementation of the Git LFS pure-SSH protocol.
+
+# go install github.com/charmbracelet/git-lfs-transfer@latest
+echo 'use mise'
