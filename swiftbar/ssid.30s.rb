bdd3cbe3a455f2a6b80b4a0d363a4b6e2ab0d400
Jxck <jxck@jxck.io>
2026-06-21 02:28:02 +0900

    [swiftbar] up ssid

--- swiftbar/ssid.30s.rb
+++ swiftbar/ssid.30s.rb
@@ -1,16 +1,17 @@
 #!/usr/bin/env ruby
-# <xbar.title>Wi-Fi SSID (ipconfig getsummary)</xbar.title>
+# <xbar.title>Wi-Fi SSID (preferred networks)</xbar.title>
 # <xbar.author>Jxck</xbar.author>
-# <xbar.desc>Get current Wi-Fi SSID using ipconfig getsummary</xbar.desc>
+# <xbar.desc>Show current Wi-Fi SSID via networksetup preferred network list</xbar.desc>
 # <xbar.version>v1.0</xbar.version>
 # <xbar.dependencies>ruby</xbar.dependencies>
 
-# output = `system_profiler SPAirPortDataType | grep 'Current Network' -A 1`
-# line = output.split("--").first.split("\n").last.strip
-output = `ipconfig getsummary en0 | grep ' SSID'`
+# macOS 14.4+ では SSID が位置情報権限なしだと <redacted> になる
+# (ipconfig / system_profiler / networksetup -getairportnetwork すべて redacted)。
+# 回避策として優先ネットワーク一覧の先頭を現在の SSID とみなす。
+# 注意: 「接続中が必ず先頭」は保証されてないが、動いてるからよしとしている。
+output = `networksetup -listpreferredwirelessnetworks en0`
 
-if output
-  puts output.split(":").last.strip
-else
-  puts ""
-end
+# 1 行目はヘッダ "Preferred networks on en0:"、2 行目以降がタブ字下げの SSID
+ssid = output.lines.drop(1).map(&:strip).reject(&:empty?).first
+
+puts ssid || ""
